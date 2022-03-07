595685d4f1f150a9cf119be5c176c9594e59f7bd
DivasGupta7 <Dvsmathur7050@gmail.com>
2022-03-07 17:57:52 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const socket = require('socket.io')
-
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'));
@@ -14,7 +13,21 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+//Get username and roomname from form and pass it to room
+app.post('/room', (req, res) => {
+    roomname = req.body.roomname;
+    username = req.body.username;
+    res.redirect(`/room?username=${username}&roomname=${roomname}`)
+})
+
+//Rooms
+app.get('/room', (req, res)=>{
+    res.render('room')
+})
+
 //Start Server
 const server = app.listen(port, () => {
     console.log(`Server Running on port ${port}`)
 })
+const io = socket(server);
+require('./utils/socket')(io);
